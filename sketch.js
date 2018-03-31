de77d9918f219983eb8df5924cc79683ab44ef23
Alex.Nathanson <nathanson.a@husky.neu.edu>
2018-03-31 12:58:47 -0400

    Changes
    Earth Roatation fixed

--- sketch.js
+++ sketch.js
@@ -3,32 +3,35 @@ let scene = 0;
 let r = 0;
 let g = 255;
 let b = 0;
+var img;
+var rotSpeed = .01
+var x = 700
+var z = 0 
+var y = 0
+var rad = 700 
 
 function setup() {
   var canvas = createCanvas(window.innerWidth, window.innerHeight - 70, WEBGL);
   canvas.parent('sketch-box');
+  img = loadImage("img/earth1.jpg");
 }
 
 function draw() {
-  background(12, 36, 97);
-  if (scene == 0) {
-    startScreen();
-  } else if (scene == 1) {
-    update();
-  } else if (scene == 2) {
-    finScreen();
-  }
-}
-
-function startScreen() {
-  background(12, 36, 97);
+ background(12, 36, 97);
   fill(r, g, b);
   translate(0, 0);
   orbitControl();
+  camera( cos(rotSpeed)* rad, y, sin(rotSpeed)* rad, 0, 0, 0, 0, 1, 0);
+  texture(img);
   sphere(250);
+  //x++;
+  //z++;
+  rotSpeed+= 0.01;
+
 }
+
 
 // click the Earth to start
 //function mousePressed() {
 
-//}
+// camera( x * cos(rotSpeed) - y * sin(rotSpeed), y * cos(rotSpeed) + x * sin(rotSpeed), z, 0, 0, 0, 0, 1, 0);
